87f7404cb429be97a560bc2d05db796697b23782
marcosfanton <marcos.fanton@ufsm.br>
2024-03-20 21:18:50 -0300

    Inclusão categoria bolsa PQ. Começo da organização da Tabela 1.

--- codigo/docentes_descricao.R
+++ codigo/docentes_descricao.R
@@ -3,30 +3,28 @@ library(tidyverse)
 library(here)
 library(ggtext)
 
-
 # Banco 
 dados <- read.csv("dados/dados_docentes.csv") |> 
+  mutate(across((where(is.character) & !matches(c("SG_ENTIDADE_ENSINO", "NM_REGIAO", "SG_UF_PROGRAMA"))),
+                ~str_to_title(.))) |> 
   mutate_if(is.character, as.factor) 
 
 # Evolução Tempo - Total ####
 dados_tempo <- dados |> 
-  group_by(AN_BASE, GENERO) |> 
+  group_by(SG_UF_PROGRAMA,   GENERO) |> 
   summarize(total = n()) |> 
   mutate(frequencia = round(total/sum(total)*100,2))
 
+?mutate_if
 
-dados_total <- dados |> 
-  filter(DS_CATEGORIA_DOCENTE == "PERMANENTE") |> 
-  group_by(AN_BASE, GENERO) |> 
-  summarize(total = n()) |> 
-  mutate(frequencia = round(total/sum(total)*100,2))
-
-# Gráfico 01 - Evolução todos ao longo do tempo####
+# Gráfico 01 - Evolução Docentes Permanentes####
 dados |> 
+  drop_na(CD_CONCEITO_PROGRAMA) |> 
+  filter(DS_CATEGORIA_DOCENTE == "PERMANENTE") |> 
   ggplot(aes(x = AN_BASE, 
              fill = GENERO)) +
   geom_bar(position = "fill")  +
-  scale_x_continuous(limits = c(2003, 2023), breaks = seq(1990, 2021, 5)) +
+  scale_x_continuous(limits = c(2012, 2023), breaks = seq(1990, 2021, 5)) +
   scale_y_continuous(labels=scales::percent, position = "right") +
   scale_fill_manual(values = c("Homem" = "#59106e",
                                "Mulher" = "#d24644")) +
@@ -40,7 +38,9 @@ dados |>
   theme(plot.title = element_markdown(face = "bold"),  
         plot.subtitle = element_markdown(hjust = 0.5),
         legend.position = "none",
-        text = element_text(size = 15))
+        text = element_text(size = 15)) +
+    facet_wrap(~NM_REGIAO)
+  
 # Salvar gráfico
 ggsave(
   "figs/graf1_total.png",
@@ -49,3 +49,52 @@ ggsave(
   height = 6,
   dpi = 1200,
   plot = last_plot())
+
+# TABELA 01 #####
+# Tabela REGIÃO 
+regiao <- dados |> 
+  group_by(NM_REGIAO) |> 
+  summarize(total = n()) |> 
+  mutate(NM_REGIAO = recode(NM_REGIAO,
+                            "NORTE" = "Norte",
+                            "NORDESTE" = "Nordeste",
+                            "CENTRO-OESTE" = "Centro-Oeste",
+                            "SUDESTE" = "Sudeste",
+                            "SUL" = "Sul"),
+         frequencia = round(total/sum(total)*100,2),
+         variavel = "Região") |> 
+    arrange(desc(total)) |> 
+    rename(categorias = NM_REGIAO) 
+
+# Tabela ESTATUTO JURÍDICO 
+estatuto <- dados |> 
+  group_by(CS_STATUS_JURIDICO, GENERO) |> 
+  summarize(total = n()) |> 
+  mutate(frequencia = round(total/sum(total)*100,2),
+         variavel = "Estatuto Jurídico") |> 
+  arrange(desc(total)) 
+
+# Tabela MODALIDADE 
+modalidade <- dados |> 
+  group_by(NM_MODALIDADE_PROGRAMA, GENERO) |> 
+  summarize(total = n()) |> 
+  mutate(frequencia = round(total/sum(total)*100,2),
+         variavel = "Modalidade") |> 
+  arrange(desc(total)) 
+
+# Tabela CATEGORIA 
+categoria <- dados |> 
+  group_by(DS_CATEGORIA_DOCENTE, GENERO) |> 
+  summarize(total = n()) |> 
+  mutate(frequencia = round(total/sum(total)*100,2),
+         variavel = "Categoria") |> 
+  arrange(desc(total)) 
+
+# Tabela CONCEITO 
+categoria <- dados |> 
+  group_by(CD_CONCEITO_PROGRAMA, GENERO) |> 
+  summarize(total = n()) |> 
+  mutate(frequencia = round(total/sum(total)*100,2),
+         variavel = "Conceito") |> 
+  arrange(desc(total)) 
+
